a440c7cde25fa00f8007b7e1bf2cb9b289337c7a
Jcolares34 <jcolares34@gmail.com>
2026-06-01 20:10:43 -0300

    feat: cria listagem de Hóspedes

---
+++ src/pages/guests/GuestListPage.vue
@@ -0,0 +1,37 @@
+<script setup>
+import BaseList from '../../components/base/BaseList.vue'
+import BaseCard from '../../components/base/BaseCard.vue'
+</script>
+
+<template>
+  <BaseList
+    title="Hóspedes"
+    resource="guests"
+    item-label="hóspede"
+    add-route-name="guests-new"
+    edit-route-name="guests-edit"
+    search-placeholder="Buscar hóspedes..."
+    :add-requires-employee="false"
+  >
+    <template #card="{ entity, askDelete, editRouteName, Actions }">
+      <BaseCard :image-url="entity.profile_photo">
+        <template #title>
+          {{ entity.first_name }} {{ entity.last_name }}
+        </template>
+
+        <template #meta>
+          <div>Doc: {{ entity.document }} · {{ entity.gender }}</div>
+          <div>{{ entity.email }} · {{ entity.phone }}</div>
+        </template>
+
+        <template #description>
+          <div>Pontos: {{ entity.loyalty_points }}</div>
+        </template>
+
+        <template #actions>
+          <Actions :entity-id="entity.id" :edit-route-name="editRouteName" @delete="askDelete" />
+        </template>
+      </BaseCard>
+    </template>
+  </BaseList>
+</template>
