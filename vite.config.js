2a66af4fdeb76edd3ab6872bf56ccfc1d75bd521
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-31 22:56:57 -0300

    feat: configura quasar vite-plugin e tema base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({ template: { transformAssetUrls } }),
+    quasar({
+      sassVariables: fileURLToPath(new URL('./src/quasar.variables.sass', import.meta.url)),
+    }),
+  ],
 })
