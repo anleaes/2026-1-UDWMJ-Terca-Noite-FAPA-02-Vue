4969fa3f91d82f14d90daed23f717c86b07fd0f0
Jcolares34 <jcolares34@gmail.com>
2026-06-01 20:53:20 -0300

    feat: cria listagem de Serviços

---
+++ src/pages/services/ServiceListPage.vue
@@ -0,0 +1,36 @@
+<script setup>
+import BaseList from '../../components/base/BaseList.vue'
+import BaseCard from '../../components/base/BaseCard.vue'
+</script>
+
+<template>
+  <BaseList
+    title="Serviços"
+    resource="services"
+    item-label="serviço"
+    add-route-name="services-add"
+    edit-route-name="services-edit"
+    search-placeholder="Buscar serviços..."
+    :filter="(entity) => entity.is_active !== false"
+  >
+    <template #card="{ entity, askDelete, editRouteName, Actions }">
+      <BaseCard :image-url="entity.photo">
+        <template #title>
+          {{ entity.name }}
+        </template>
+
+        <template #meta>
+          <div>{{ entity.category }} · R$ {{ entity.price }}</div>
+        </template>
+
+        <template #description>
+          <div>{{ entity.description }}</div>
+        </template>
+
+        <template #actions>
+          <Actions :entity-id="entity.id" :edit-route-name="editRouteName" @delete="askDelete" />
+        </template>
+      </BaseCard>
+    </template>
+  </BaseList>
+</template>
